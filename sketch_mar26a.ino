d8ce8227d6c1f994050a60ad80bfcea05cbc155c
Corto Beau <150670973+cortobeau@users.noreply.github.com>
2024-03-31 23:34:12 +0200

    Commentaires Arduino

--- sketch_mar26a.ino
+++ sketch_mar26a.ino
@@ -1,19 +1,19 @@
 #include <Servo.h>
 
-constexpr uint8_t JOYSTICK_X = A5;
-constexpr uint8_t JOYSTICK_Y = A4;
-
 #define VIN 5 // V power voltage
 #define R 10000 //ohm resistance value
 
-#define LASER 12
+// pins
+constexpr uint8_t JOYSTICK_X = A5;
+constexpr uint8_t JOYSTICK_Y = A4;
+constexpr uint8_t LASER = 12;
+constexpr uint8_t SERVO1 = 5;
+constexpr uint8_t SERVO2 = 6;
 
 Servo servo1;
-#define SERVO1 5
-
 Servo servo2;
-#define SERVO2 6
 
+// conversion "valeur pin photorésistance -> lumens"
 int sensorRawToPhys(int raw){
   // Conversion rule
   float Vout = float(raw) * (VIN / float(1023));// Conversion analog to voltage
@@ -35,8 +35,8 @@ void setup() {
   pinMode(JOYSTICK_Y, INPUT);
 }
 
-template <typename T>
-T clamp(T value, T min, T max) {
+// contraint une valeur à un intervalle [min; max]
+template <typename T> T clamp(T value, T min, T max) {
   if (value < min) {
     return min;
   } else if (value > max) {
@@ -46,30 +46,34 @@ T clamp(T value, T min, T max) {
   }
 }
 
-unsigned long lastTime = 0;
+unsigned long lastJoystickRead = 0;
 
+// délai nécessaire pour éviter que le joystick soit trop sensible
 constexpr unsigned long JOYSTICK_INPUT_DELAY = 75;
 
+// état global de l'activité
 enum GlobalState {
-  Inactive,
-  Start,
-  Active,
+  Inactive = 0, // inactif - l'utilisateur n'a pas touché au joystick depuis un moment
+  Start = 1, // démarrage - l'utilisateur vient de toucher au joystick
+  Active = 2, // actif
 };
 
+// état d'une zone en particulier
 enum ZoneState {
-  Blank,
-  Scanned,
-  Probed
+  Blank = 0, // la zone n'est pas explorée
+  Scanned = 1, // la zone a été scannée
+  Probed = 2 // une balise a été installée dans la zone
 };
 
+// défini une zone scannable
 struct Zone {
-  uint8_t photo_cell_pin;
+  uint8_t photo_cell_pin; // pin sur lequel la photorésistance de la zone est connecté
 
-  ZoneState state;
+  ZoneState state; // état de la zone
 
-  unsigned long lastStateChange;
+  unsigned long lastStateChange; // dernière fois que l'état a changé
 
-  bool isCandidate;
+  bool isCandidate; // une balise peut être posée
 };
 
 #define NB_ZONES 4
@@ -83,6 +87,8 @@ Zone zones[NB_ZONES] = {
 
 constexpr unsigned long INACTIVITY_DELAY = 30000;
 
+constexpr unsigned long ZONE_STATE_CHANGE_DELAY = 1000;
+
 GlobalState globalState = Inactive;
 
 unsigned long lastGlobalStateChange = 0;
@@ -92,11 +98,13 @@ unsigned long lastActive = 0;
 void loop() {
   unsigned long now = millis();
 
+  // on allume le laser si on est pas en inactif
   digitalWrite(LASER, globalState == Inactive ? LOW : HIGH);
 
-  if (now - lastTime >= JOYSTICK_INPUT_DELAY) {
-    lastTime = now;
+  if (now - lastJoystickRead >= JOYSTICK_INPUT_DELAY) {
+    lastJoystickRead = now;
 
+    // calcul du delta a appliquer aux moteurs
     int dx = analogRead(JOYSTICK_X) - 511;
     if (dx < 30 && dx > -30) {
       dx = 0;
@@ -119,6 +127,7 @@ void loop() {
       servo2.write(servo2.read() + dy);
     }
 
+    // si le delta a appliquer est non nul
     if (dx != 0 || dy != 0) {
       if (globalState == Inactive) {
         globalState = Start;
@@ -130,6 +139,8 @@ void loop() {
     }
   }
 
+  // si l'utilisateur a interragi pour la dernière fois il y a plus de
+  // INACTIVITY_DELAY secondes et qu'on est pas déjà en inactif, on passe en inactif.
   if (globalState != Inactive && now - lastActive > INACTIVITY_DELAY) {
     globalState = Inactive;
 
@@ -140,17 +151,26 @@ void loop() {
 
     lastGlobalStateChange = now;
 
+    // remise à zéro des servos
     servo1.write(93);
     servo2.write(93);
   }
 
-  constexpr unsigned long ZONE_STATE_CHANGE_DELAY = 1000;
-
+  // si on est en état "démarrage" depuis plus de 15 secondes,
+  // on passe en "actif" (pour masquer la vidéo introductive).
   if (globalState == Start && now - lastGlobalStateChange > 15000) {
     globalState = Active;
     lastGlobalStateChange = now;
     lastActive = now;
   }
+
+  // afficher l'état de l'activité au format suivant :
+
+  // a,b,c,d,e
+  //
+  // avec :
+  // a = état global de l'application
+  // b,c,d,e = état de chaque zone
 
   Serial.print(globalState);
 
@@ -161,11 +181,16 @@ void loop() {
 
     int lumen = sensorRawToPhys(analogRead(zone.photo_cell_pin));
 
+    // si la valeur mesurée en lumen est au delà de 2000, alors
+    // le laser vient de passer sur le capteur
     if (lumen > 2000 && now - zone.lastStateChange > ZONE_STATE_CHANGE_DELAY) {
       zone.lastStateChange = now;
 
+      // si la zone n'est pas découverte, on la passe en "scannée"
       if (zone.state == Blank) {
         zone.state = Scanned;
+      // si la zone est déjà scannée et qu'elle est candidate
+      // au replentage, on passe en "balisée"
       } else if (zone.isCandidate && zone.state == Scanned) {
         zone.state = Probed;
       }
