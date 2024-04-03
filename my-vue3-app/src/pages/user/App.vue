0202996a2c0e61a72e0d0349dd75e67139d23526
Jie ZHANG <zhangjiedjj@gmail.com>
2024-04-04 04:34:26 +0800

    Rebuild for deployment

--- my-vue3-app/src/pages/user/App.vue
+++ my-vue3-app/src/pages/user/App.vue
@@ -61,7 +61,7 @@
 
 <script setup>
 import { io } from 'socket.io-client'
-const socket = io('ws://hyblab.polytech.univ-nantes.fr:3010/');
+const socket = io('ws://hyblab.polytech.univ-nantes.fr');
 import { ref } from 'vue';
 import im1 from './/assets/images/i1.png'
 import im2 from './/assets/images/i2.png'
